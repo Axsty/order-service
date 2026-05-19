e951cd9b1a24bb218397ae2aae73ae4e25556806
Axsty <axelstaaffriborg@gmail.com>
2026-05-19 10:53:20 +0200

    Initial commit

---
+++ src/test/java/se/iths/axel/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.axel.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
