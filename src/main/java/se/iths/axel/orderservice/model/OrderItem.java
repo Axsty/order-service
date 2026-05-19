cce56e1d02916eea1c714fbbefd784c7286d6ea6
Axsty <axelstaaffriborg@gmail.com>
2026-05-19 12:12:38 +0200

    Order models.

---
+++ src/main/java/se/iths/axel/orderservice/model/OrderItem.java
@@ -0,0 +1,23 @@
+package se.iths.axel.orderservice.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private BigDecimal price;
+
+    private int quantity;
+}
