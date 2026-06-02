f7d47081767c625895e13120c5dbef7da58833de
Axsty <axelstaaffriborg@gmail.com>
2026-06-02 09:20:39 +0200

    exception handling

--- src/main/java/se/iths/axel/orderservice/client/ProductClient.java
+++ src/main/java/se/iths/axel/orderservice/client/ProductClient.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.server.ResponseStatusException;
 import se.iths.axel.orderservice.dto.ProductInfo;
 import se.iths.axel.orderservice.dto.ProductStockRequest;
 
@@ -16,12 +18,16 @@ public class ProductClient {
     private final RestClient restClient;
 
     public List<ProductInfo> decreaseStock(List<ProductStockRequest> items, String bearerToken) {
-        return restClient.post()
-                .uri("/products/stock/decrease")
-                .header("Authorization", bearerToken)
-                .body(items)
-                .retrieve()
-                .body(new ParameterizedTypeReference<>() {
-                });
+        try {
+            return restClient.post()
+                    .uri("/products/stock/decrease")
+                    .header("Authorization", bearerToken)
+                    .body(items)
+                    .retrieve()
+                    .body(new ParameterizedTypeReference<>() {
+                    });
+        } catch (RestClientResponseException e) {
+            throw new ResponseStatusException(e.getStatusCode(), e.getResponseBodyAsString());
+        }
     }
 }
